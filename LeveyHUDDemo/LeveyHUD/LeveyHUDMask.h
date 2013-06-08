667fbe14672c97c9047e212f119c06b540f7ec42
Bill Cheng <so89898@gmail.com>
2013-06-08 15:33:39 +0800

    Improve code
    Combine HUDTopMask and HUDBotMask to HUDMask. Using transform to
    present two type Masks.

--- LeveyHUDDemo/LeveyHUD/LeveyHUDMask.h
+++ LeveyHUDDemo/LeveyHUD/LeveyHUDMask.h
@@ -1,13 +1,15 @@
 //
-//  LeveyHUDBottomMask.h
+//  LeveyHUDMask.h
 //  LeveyHUD
 //
 //  Created by Levey on 12/12/11.
 //  Copyright (c) 2011 lunaapp.com. All rights reserved.
 //
+//  Amend by so898 on 20/12/2012
+//  Copyright (c) 2012 R³ Studio. All rights reserved.
 
 #import <UIKit/UIKit.h>
 
-@interface LeveyHUDBottomMask : UIView
+@interface LeveyHUDMask : UIView
 
 @end
